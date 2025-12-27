0338af6a17884d58d86f0481b0ee96da976e7ada
Andrea Espada Murguia <aespada0@uoc.edu>
2025-12-27 17:33:23 +0100

    limpieza del csv de kaggle

--- source/main.R
+++ source/main.R
@@ -2,3 +2,7 @@ library(tidyverse)
 
 # 1. Ejercutar limpieza del script original 
 source("source/scripts/limpiar_csv_practica1.R")
+
+# 2. Ejecutar lipieza del script de kaggle
+# https://www.kaggle.com/datasets/iabhishekofficial/mobile-price-classification?resource=download
+source("source/scripts/limpiar_csv_productos1.R")
---
+++ source/scripts/limpiar_csv_productos1.R
@@ -0,0 +1,36 @@
+df_kaggle_train <- read.csv("source/dataset/productos1/train.csv")
+df_kaggle_test <- read.csv("source/dataset/productos1/test.csv")
+
+# para juntar los 2 datasets, tenemos que tener en cuenta que test no tiene price_range
+df_kaggle_test$price_range <- NA
+
+# juntamos los 2 datasets
+df_kaggle <- bind_rows(df_kaggle_train, df_kaggle_test)
+
+# miramos estructura
+print(str(df_kaggle))
+print(summary(df_kaggle))
+print(colSums(is.na(df_kaggle)))
+
+# variable categorica
+df_kaggle$price_range <- as.factor(df_kaggle$price_range)
+
+# ponemos como numeric lo que no sea numeric
+df_kaggle <- df_kaggle %>%
+  mutate(across(-price_range, as.numeric))
+
+# imputacion
+df_kaggle <- df_kaggle %>%
+  mutate(
+    across(
+      where(is.numeric),
+      ~ ifelse(is.na(.), median(., na.rm = TRUE), .)
+    )
+  )
+
+# guardamos el dataset limpio
+write.csv(
+  df_kaggle,
+  "source/dataset_final/productos1/productos1.csv",
+  row.names = FALSE
+)
