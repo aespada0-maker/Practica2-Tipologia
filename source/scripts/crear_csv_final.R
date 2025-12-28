2cccede56e4ff211d7eeff516b00782e020f7918
Andrea Espada Murguia <aespada0@uoc.edu>
2025-12-28 16:07:38 +0100

    columnas finales

---
+++ source/scripts/crear_csv_final.R
@@ -0,0 +1,8 @@
+columnas_finales <- c(
+  "marca", "modelo", "precio",
+  "valoracion_media",
+  "val_1", "val_2", "val_3", "val_4", "val_5",
+  "ram_gb", "battery_mah", "screen_size",
+  "num_cores", "os", "year"
+)
+
