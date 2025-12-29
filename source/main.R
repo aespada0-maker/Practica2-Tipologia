e4ab1bc6b11b2ae9a36c81eb68af1a54a38f4811
pgomezrev <pgomezrev@uoc.edu>
2025-12-29 17:11:56 +0100

    Creación de script final juntar todos los datos

--- source/main.R
+++ source/main.R
@@ -4,20 +4,15 @@ library(dplyr)
 # 1. Ejercutar limpieza del script original 
 source("source/scripts/limpiar_csv_practica1.R")
 
-# 2. Ejecutar lipieza del script de kaggle
-# https://www.kaggle.com/datasets/iabhishekofficial/mobile-price-classification?resource=download
-source("source/scripts/limpiar_csv_productos1.R")
-
-# 3. Ejecutar lipieza del script de www.opendatabay.com
+# 2. Ejecutar lipieza del script de www.opendatabay.com
 # https://www.opendatabay.com/data/consumer/a16cb863-a839-4245-800f-37ef284b2883
 source("source/scripts/limpiar_csv_productos2.R")
 
-# 4. Ejecutar limpieza del script externo
+# 3. Ejecutar limpieza del script externo
 source("source/scripts/limpiar_csv_externo.R")
 
 lapply(
   list(
-    df_kaggle,
     df_oppendatabay,
     df_pract1,
     df_raw
--- source/scripts/crear_csv_final.R
+++ source/scripts/crear_csv_final.R
@@ -1,3 +1,23 @@
+#Función base para homogeneizar todos los DF
+
+homogeneizar <- function(df,mapeo,columnas_finales){
+  
+  for (i in seq_along(mapeo)){
+    if(names(mapeo)[i] %in% names(df)){
+      names(df)[names(df) == names(mapeo)[i]] <- mapeo[i]
+    }
+  }
+  #Creamos columnas faltantes
+  faltantes <- setdiff(columnas_finales,names(df))
+  for (col in faltantes) {
+    df[[col]] <- NA
+  }
+  
+  #Nos quedamos con el esquema final
+  df <- df[,columnas_finales]
+  return(df)
+}
+
 columnas_finales <- c(
   "marca", "modelo", "precio",
   "valoracion_media",
@@ -6,3 +26,60 @@ columnas_finales <- c(
   "num_cores", "os", "year"
 )
 
+mapeo_pract1 <- c(
+  "marca" = "marca",
+  "modelo" = "modelo",
+  "precio" = "precio",
+  "valoracion_media" = "valoracion_media",
+  "val_1" = "val_1",
+  "val_2" = "val_2",
+  "val_3" = "val_3",
+  "val_4" = "val_4",
+  "val_5" = "val_5"
+)
+
+mapeo_oppendatabay <- c(
+    "brand_name" = "marca",
+    "model" = "modelo",
+    "price" = "precio",
+    "avg_rating" = "valoracion_media",
+    "ram_capacity" = "ram_gb",
+    "battery_capacity" = "battery_mah",
+    "screen_size" = "screen_size",
+    "num_cores" = "num_cores",
+    "os" = "os"
+  )
+
+mapeo_raw <- c(
+  "Company.Name" = "marca",
+  "Model.Name" = "modelo",
+  "RAM" = "ram_gb",
+  "Battery.Capacity" = "battery_mah",
+  "Screen.Size" = "screen_size",
+  "Launched.Year" = "year",
+  "Launched.Price..USA." = "precio"
+)
+
+normalizar_texto <- function(x){
+  tolower(trimws(x))
+}
+#Homogeneizamos los dataFrames
+prac1_h <- homogeneizar(df_pract1,mapeo_pract1,columnas_finales)
+oppendatabay_h <- homogeneizar(df_oppendatabay,mapeo_oppendatabay,columnas_finales)
+raw_h <- homogeneizar(df_raw,mapeo_raw,columnas_finales)
+
+#Aplicamos normalización del texto
+prac1_h$marca <- normalizar_texto(prac1_h$marca)
+prac1_h$modelo <- normalizar_texto(prac1_h$modelo)
+
+oppendatabay_h$marca <- normalizar_texto(oppendatabay_h$marca)
+oppendatabay_h$modelo <- normalizar_texto(oppendatabay_h$modelo)
+
+raw_h$marca <- normalizar_texto(raw_h$marca)
+raw_h$modelo <- normalizar_texto(raw_h$modelo)
+
+dataset_final_raw <- rbind(
+  prac1_h,
+  oppendatabay_h,
+  raw_h
+)
